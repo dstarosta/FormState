f7cdfb3791703a84537a4fa421fefeac61ba80ea
Dmitry S <dmitry@starosta.net>
2026-03-20 15:31:23 -0400

    Improvements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import sonarjs from 'eslint-plugin-sonarjs';
+import react from 'eslint-plugin-react-hooks';
 import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
@@ -16,6 +17,7 @@ export default defineConfig([
       tseslint.configs.strictTypeChecked,
       eslintPluginUnicorn.configs.all,
       sonarjs.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       eslintConfigPrettier,
     ],
